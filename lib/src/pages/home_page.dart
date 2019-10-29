24c98f4e30d99c6b94fab228f7e544144a5cc76f
Luis Alvarado <nsaid569@gmail.com>
2019-10-29 17:20:12 -0300

    Se agrega widget de columna

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  final estiloTexto = TextStyle(fontSize: 25.0);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,7 +13,13 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Hola Mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clicks:', style: estiloTexto),
+            Text('0', style: estiloTexto)
+          ],
+        )
       ),
     );
   }
